63f874e19ec9a67435d858de4e2c1240e6cf8d20
Chocolateboy1191 <153948180+Chocolateboy1191@users.noreply.github.com>
2026-03-24 18:33:07 +0700

    Remove commented-out code in engine.js

--- modules/engine.js
+++ modules/engine.js
@@ -2,17 +2,3 @@ import { perception } from "./perception.js";
 import { geometry } from "./geometry.js";
 import { field } from "./field.js";
 import { meta } from "./meta.js";
-
-export async function runMCE(input) {
-  const p = perception(input);
-  const g = await geometry(p);
-  const f = await field(g);
-  const m = meta(f);
-
-  return {
-    perception: p,
-    geometry: g,
-    field: f,
-    meta: m
-  };
-}
