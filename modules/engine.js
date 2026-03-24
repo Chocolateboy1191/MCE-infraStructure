897d435aab93e17de1d61df06b5ef1442fec9dc4
Chocolateboy1191 <153948180+Chocolateboy1191@users.noreply.github.com>
2026-03-24 18:43:22 +0700

    Refactor imports to CommonJS syntax and add runMCE

--- modules/engine.js
+++ modules/engine.js
@@ -1,4 +1,20 @@
-import { perception } from "./perception.js";
-import { geometry } from "./geometry.js";
-import { field } from "./field.js";
-import { meta } from "./meta.js";
+const { perception } = require("./perception.js");
+const { geometry } = require("./geometry.js");
+const { field } = require("./field.js");
+const { meta } = require("./meta.js");
+
+async function runMCE(input) {
+  const p = perception(input);
+  const g = await geometry(p);
+  const f = await field(g);
+  const m = meta(f);
+
+  return {
+    perception: p,
+    geometry: g,
+    field: f,
+    meta: m
+  };
+}
+
+module.exports = { runMCE };
